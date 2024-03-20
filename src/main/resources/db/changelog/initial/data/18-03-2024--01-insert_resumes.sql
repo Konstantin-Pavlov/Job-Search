549a29973c12a83cf2767d8bb0e011ffbbf33706
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-20 21:56:59 +0600

    add applicant_id column;

--- src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_resumes.sql
+++ src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_resumes.sql
@@ -1,4 +1,4 @@
-INSERT INTO resumes (name, category_id, salary, isActive, createdDate, updateTime)
-VALUES ('Software Engineer', 1, 80000.00, true, '2024-03-12', '2024-03-12'),
-       ('Marketing Specialist', 2, 60000.00, false, '2024-03-12', '2024-03-12'),
-       ('driver', 3, 45000.00, true, '2024-03-12', '2024-03-12')
+INSERT INTO resumes (APPLICANT_ID, name, category_id, salary, isActive, createdDate, updateTime)
+VALUES (1, 'Software Engineer', 1, 80000.00, true, '2024-03-12', '2024-03-12'),
+       (3, 'Marketing Specialist', 2, 60000.00, false, '2024-03-12', '2024-03-12'),
+       (2, 'driver', 3, 45000.00, true, '2024-03-12', '2024-03-12')
