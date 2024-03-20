7b78bb7487bfbf7fd5621fe81446f86c8700d1b9
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-20 21:57:52 +0600

    change respondedApplicantId column

--- src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_messages.sql
+++ src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_messages.sql
@@ -1,6 +1,6 @@
 INSERT INTO messages (respondedApplicantId, content, timestamp)
 VALUES
-    (4, 'This is the first message', '2024-03-12 10:00:00'),
-    (5, 'This is the second message', '2024-03-13 11:00:00'),
-    (6, 'This is the third message', '2024-03-14 12:00:00');
+    (2, 'This is the first message', '2024-03-12 10:00:00'),
+    (1, 'This is the second message', '2024-03-13 11:00:00'),
+    (3, 'This is the third message', '2024-03-14 12:00:00');
 
