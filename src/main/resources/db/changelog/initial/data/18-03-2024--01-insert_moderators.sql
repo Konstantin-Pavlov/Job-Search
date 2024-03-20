0fe281c1a733503cc0422e456c181497bb262995
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-21 00:03:15 +0600

    add and update new table - moderators

---
+++ src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_moderators.sql
@@ -0,0 +1,7 @@
+INSERT INTO moderators (email, password)
+VALUES ('alice.moderator@example.com', 'P@ssw0rd1'),
+       ('bob_moderator@gmail.com', 'Moderator123'),
+       ('charlie_m@example.org', 'SecurePassword'),
+       ('david_moderator@company.com', 'SecretPass!23'),
+       ('emma_moderator@yahoo.com', 'Pass123word'),
+       ('frank_moderator@hotmail.com', 'StrongP@ssw0rd');
