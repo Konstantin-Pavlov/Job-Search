a3e8ede58a04415b03f5c13bdafaca7deac13f6b
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-21 19:35:32 +0600

    delete

--- src/main/resources/db/changelog/initial/data/18-03-2024--01-insert_work_experience_info.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO work_experience_info (applicantId, name, categoryId, salary, isActive, createdDate, updateTime)
-VALUES
-    (1, 'Work Experience 1', 4, 50000, true, '2024-03-12 09:00:00', '2024-03-12 09:30:00'),
-    (2, 'Work Experience 2', 2, 60000, true, '2024-03-12 10:00:00', '2024-03-12 10:30:00'),
-    (1, 'Work Experience 3', 1, 55000, true, '2024-03-12 11:00:00', '2024-03-12 11:30:00');
