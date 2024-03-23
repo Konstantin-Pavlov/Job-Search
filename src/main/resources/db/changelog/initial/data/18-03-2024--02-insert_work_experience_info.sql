119a4e3aaee64b63794116b60849bcdceb486576
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-23 22:43:01 +0600

    fixed

--- src/main/resources/db/changelog/initial/data/18-03-2024--02-insert_work_experience_info.sql
+++ src/main/resources/db/changelog/initial/data/18-03-2024--02-insert_work_experience_info.sql
@@ -1,5 +1,5 @@
 INSERT INTO work_experience_info (resumeid, years, company_name, position, responsibilities)
 VALUES
     (1, 3, 'company1', 'position', 'work process'),
-    (3, 5, 'company2', 'position', 'work process'),
-    (6, 1, 'company3', 'position', 'work process');
+    (2, 5, 'company2', 'position', 'work process'),
+    (3, 1, 'company3', 'position', 'work process');
