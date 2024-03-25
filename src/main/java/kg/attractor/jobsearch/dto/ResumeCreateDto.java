a1f1dd953fdae8fb7b4929b8d259a86741d62d80
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-26 00:39:22 +0600

    add new class ResumeCreateDto

---
+++ src/main/java/kg/attractor/jobsearch/dto/ResumeCreateDto.java
@@ -0,0 +1,19 @@
+package kg.attractor.jobsearch.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE) // all fields are private
+public class ResumeCreateDto {
+    @Email
+    String email;
+}
