9e8457f2fcd6ddbb724ed190a09f12c9665398a1
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-04-01 01:13:33 +0600

    refactor

--- src/main/java/kg/attractor/jobsearch/config/SwaggerConfig.java
+++ src/main/java/kg/attractor/jobsearch/config/SwaggerConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 import java.util.Map;
