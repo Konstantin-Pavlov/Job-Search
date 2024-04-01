1bc9a67db1ca6ee1d81b4d92d7cde03f8fee5a73
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-04-01 22:40:35 +0600

    add SecurityConfig class

---
+++ src/main/java/kg/attractor/jobsearch/config/SecurityConfig.java
@@ -0,0 +1,88 @@
+package kg.attractor.jobsearch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final PasswordEncoder encoder;
+    private final DataSource dataSource;
+
+    private static final String USER_QUERY =
+            """
+                            SELECT EMAIL, PASSWORD, ENABLED
+                            FROM USERS
+                            WHERE EMAIL=?
+                    """;
+    private static final String AUTHORITIES_QUERY =
+            """
+                    select USERS.EMAIL, A.ROLE
+                    from USERS
+                             inner join ROLES UA on USERS.ID = UA.USER_ID
+                             inner join AUTHORITIES A on A.ID = UA.AUTHORITY_ID
+                    where EMAIL=?;
+                                """;
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("admin"))
+//                .roles("ADMIN")
+//                .authorities("FULL")
+//                .build();
+//
+//        UserDetails guest = User.builder()
+//                .username("guest")
+//                .password(encoder.encode("123"))
+//                .roles("GUEST")
+//                .authorities("READ_ONLY")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, guest);
+//    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(USER_QUERY)
+                .authoritiesByUsernameQuery(AUTHORITIES_QUERY)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/users/{id}").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users/add").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasAnyAuthority("ADMIN", "USER")
+//                        .requestMatchers("/quizzes/**").hasAuthority("ADMIN")
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
