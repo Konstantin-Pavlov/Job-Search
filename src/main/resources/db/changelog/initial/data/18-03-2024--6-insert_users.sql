1f7202e580014088979a4a88a7b6bdb8ccbeac51
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-04-01 22:39:34 +0600

    removed role_id; changed some passwords

---
+++ src/main/resources/db/changelog/initial/data/18-03-2024--6-insert_users.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO users (NAME, AGE, EMAIL, PASSWORD, PHONE_NUMBER, AVATAR, ACCOUNT_TYPE, ENABLED)
+VALUES
+    ('Alice Johnson', 29, 'alice@example.com', '$2a$10$ESXS5rMP5nfdkFqP5RC8vuUK1v4jWUIrLPxz9dIdWQi2NRzF0Y8AW', '1234567890', 'avatar1.jpg', 'applicant', true), -- alice123
+    ('Charlie Brown', 40, 'charlie@example.com', '$2a$10$7/0uDHO8ctEwxaVvukRcWeDkDhIQkrHfSSP8zvPiOVBCH3YGO61v2', '5555555555', 'avatar3.jpg', 'employer', true), -- charlie123
+    ('John Doe', 35, 'john.doe@example.com', '$2a$10$BweqRbyr9054HudG406oqeA.XBvWpuUrKaVODh4T8K.xLehPvMHvy', '9876543210', 'avatar2.jpg', 'applicant', true), --john123
+    ('Emma Smith', 28, 'emma@example.com', '$2a$10$Bd21s5ZThg0z/vtxCB0z9eKYLlSmXUQIklAQEZQK15sr/DoSUQsSe', '4444444444', 'avatar4.jpg', 'employer', true), --emma123
+    ('Michael Johnson', 45, 'michael@example.com', 'michael123', '3333333333', 'avatar5.jpg', 'applicant', true),
+    ('Sarah Lee', 33, 'sarah@example.com', 'sarah123', '6666666666', 'avatar6.jpg', 'employer', true),
+    ('David Brown', 38, 'david@example.com', 'david123', '7777777777', 'avatar7.jpg', 'applicant', true),
+    ('Emily Wilson', 31, 'emily@example.com', 'emily123', '8888888888', 'avatar8.jpg', 'employer', true),
+    ('Ryan Miller', 42, 'ryan@example.com', 'ryan123', '9999999999', 'avatar9.jpg', 'applicant', true),
+    ('Olivia Taylor', 27, 'olivia@example.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', '2222222222', 'avatar10.jpg', 'employer', true), -- qwe
+    ('Daniel Martinez', 36, 'daniel@example.com', 'daniel123', '1111111111', 'avatar11.jpg', 'applicant', true),
+    ('Sophia Anderson', 30, 'sophia@example.com', 'sophia123', '1010101010', 'avatar12.jpg', 'employer', true),
+    ('James Clark', 39, 'james@example.com', 'james123', '1212121212', 'avatar13.jpg', 'applicant', true),
+    ('Ava Harris', 26, 'ava@example.com', 'ava123', '1313131313', 'avatar14.jpg', 'employer', true),
+    ('Matthew Young', 43, 'matthew@example.com', 'matthew123', '1414141414', 'avatar15.jpg', 'applicant', true),
+    ('Isabella Lopez', 32, 'isabella@example.com', 'isabella123', '1515151515', 'avatar16.jpg', 'employer', true),
+    ('Christopher King', 37, 'christopher@example.com', 'christopher123', '1616161616', 'avatar17.jpg', 'applicant', true),
+    ('Emma Johnson', 29, 'emma.johnson@example.com', 'emma123', '1717171717', 'avatar18.jpg', 'employer', true),
+    ('Alexander Davis', 44, 'alexander@example.com', 'alexander123', '1818181818', 'avatar19.jpg', 'applicant', true),
+    ('Madison Moore', 28, 'madison@example.com', 'madison123', '1919191919', 'avatar20.jpg', 'employer', true);
