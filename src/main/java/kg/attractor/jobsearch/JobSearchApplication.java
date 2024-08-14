b8f57ba9660818c16b0efa9b65678c1f79af8663
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-08-14 12:08:22 +0600

    add annotation to avoid mapping error

--- src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
+++ src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
@@ -2,8 +2,16 @@ package kg.attractor.jobsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+
+// to avoid this error:
+// org.springframework.beans.factory.BeanDefinitionStoreException: I/O failure while processing configuration class [kg.attractor.jobsearch.mapper.CategoryMapperImpl]
+// CategoryMapperImpl is in another branch (hibernate branch) at the moment
+@ComponentScan(basePackages = "kg.attractor.jobsearch",
+		excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "kg\\.attractor\\.jobsearch\\.mapper\\..*"))
 public class JobSearchApplication {
 
 	public static void main(String[] args) {
