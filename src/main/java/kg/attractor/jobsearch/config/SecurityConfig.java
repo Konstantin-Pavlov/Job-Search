cc7451042a203d978bd5a856863a976e4ef34c43
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-08-15 17:35:36 +0600

    temp

--- src/main/java/kg/attractor/jobsearch/config/SecurityConfig.java
+++ src/main/java/kg/attractor/jobsearch/config/SecurityConfig.java
@@ -16,57 +16,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
-    //    private final PasswordEncoder encoder;
-    private final DataSource dataSource;
+public class SecurityConfig  {
     private final CustomUserDetailsServiceImpl customUserDetailsServiceImpl;
-
-    private static final String USER_QUERY =
-            """
-                            SELECT EMAIL, PASSWORD, ENABLED
-                            FROM USERS
-                            WHERE EMAIL=?
-                    """;
-    private static final String AUTHORITIES_QUERY =
-            """
-                    select USERS.EMAIL, A.ROLE
-                    from USERS
-                             inner join ROLES UA on USERS.ID = UA.USER_ID
-                             inner join AUTHORITIES A on A.ID = UA.AUTHORITY_ID
-                    where EMAIL=?;
-                    """;
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(encoder.encode("admin"))
-//                .roles("ADMIN")
-//                .authorities("FULL")
-//                .build();
-//
-//        UserDetails guest = User.builder()
-//                .username("guest")
-//                .password(encoder.encode("123"))
-//                .roles("GUEST")
-//                .authorities("READ_ONLY")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, guest);
-//    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(USER_QUERY)
-                .authoritiesByUsernameQuery(AUTHORITIES_QUERY)
+                .userDetailsService(customUserDetailsServiceImpl)
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
@@ -86,37 +45,17 @@ public class SecurityConfig {
                         .permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/swagger-ui/**").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/vacancies").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/auth/register").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/auth/profile").fullyAuthenticated()
-//                                .requestMatchers(HttpMethod.GET, "/applicant/vacancies").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/applicant/get-user-vacancies/{user_id}").hasAnyAuthority("USER", "ADMIN")
-//                                .requestMatchers(HttpMethod.GET, "/applicant/avatar/{userId}").hasAnyAuthority("USER", "ADMIN")
-//                                .requestMatchers(HttpMethod.GET, "/users/{id}").hasAuthority("ADMIN")
-//                                .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority("ADMIN")
-////                        .requestMatchers("/quizzes/**").hasAuthority("ADMIN")
-//                                .requestMatchers(HttpMethod.GET, "/users").permitAll()
-//                                .requestMatchers(HttpMethod.POST, "/applicant/add").permitAll()
-//                                .requestMatchers(HttpMethod.POST, "/applicant/add-with-avatar").permitAll()
-//                                .requestMatchers(HttpMethod.POST, "/applicant/{userId}/upload-avatar").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.POST, "/applicant/resume").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.POST, "/applicant/vacancy/{vacancyId}/apply").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.PUT, "/applicant/resume/{id}").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.DELETE, "/applicant/resume/{id}").hasAnyAuthority("ADMIN", "USER")
-//
-//                                .requestMatchers(HttpMethod.GET, "/employer/resumes").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/employer/resumes/category/{category}").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/employer/vacancy/{vacancyId}/applicants").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.POST, "/employer/add").permitAll()
-//                                .requestMatchers(HttpMethod.POST, "/employer/vacancy").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.PUT, "/employer/vacancy/{id}").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers(HttpMethod.DELETE, "/employer/vacancy/{id}").hasAnyAuthority("ADMIN", "USER")
-
-                                .anyRequest().authenticated()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/").permitAll() // Allow access to the root URL
+                        .requestMatchers("/static/**").permitAll() // Allow access to static resources
+                        .requestMatchers(HttpMethod.GET, "/vacancies").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/profile").hasAuthority("USER")
+                        .anyRequest().authenticated()
                 );
         return http.build();
     }
+
 }
