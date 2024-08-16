d798b6eb10df054dbb9b6e3a790baea718867914
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-08-16 11:49:23 +0600

    del unused imports

--- src/main/java/kg/attractor/jobsearch/config/AppConfig.java
+++ src/main/java/kg/attractor/jobsearch/config/AppConfig.java
@@ -1,20 +1,16 @@
 package kg.attractor.jobsearch.config;
 
-import kg.attractor.jobsearch.service.impl.CustomUserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
-    private final CustomUserDetailsServiceImpl customUserDetailsServiceImpl;
 
     @Bean
     public PasswordEncoder encoder() {
